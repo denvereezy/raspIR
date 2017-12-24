ae7eb39f4e7f16bd862d483ef005ececcecd48c0
denver <denver@projectcodex.co>
2017-12-24 20:00:00 +0200

    app

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    commander = require('./commander'),
+    app = express(),
+    router = express.Router();
+
+
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.json());
+
+router.post('/payload', (req, res) => {
+    var data = req.body.data;
+    commander.command(data);
+    res.sendStatus(204);
+});
+
+app.use('/api', router);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
